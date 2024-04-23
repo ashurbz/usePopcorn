3433b48b881dc4c397e7185078f2921bb8f47f56
Ashish <rbzashu1@gmail.com>
2024-04-23 10:49:40 +0530

    focus on search bar using useRef

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./common.css";
 
 const SearchBar = ({ query, onSearch }) => {
   const handleOnChange = (e) => {
     onSearch(e.target.value);
   };
+
+  const element = useRef(null);
+
+  useEffect(() => {
+    element.current.focus();
+  }, []);
+
+  console.log(element);
 
   return (
     <div>
@@ -14,6 +22,7 @@ const SearchBar = ({ query, onSearch }) => {
           placeholder="Search for movies"
           value={query}
           onChange={handleOnChange}
+          ref={element}
         />
       </div>
     </div>
